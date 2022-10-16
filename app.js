36635e33eb21eed8ff3299f95ef10a549eef62df
lomo007 <eragon66997@gmail.com>
2022-10-16 20:40:17 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const db = mongoose.connection
 
 const bodyParser = require('body-parser')
 
+
 let hint = "請輸入餐廳、分類"
 let hintError = "查無餐廳, 請重新輸入"
 
@@ -36,6 +37,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
+    .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
@@ -133,6 +135,25 @@ app.get('/search', (req, res) => {
     .then((restaurants) => res.render('index', { restaurants, keyword }))  //大括弧內的陣列依序輸出
     .catch(error => console.error(error))
 
+})
+
+app.get('/list', (req, res) => {
+  const { keyword, sort, order, title } = req.query
+  return Restaurant.find()
+    .sort({ [sort]: order })
+    .lean()
+    .then(restaurants => {
+      // 先打包所有restaurants再過濾包含keyword的list
+      const filterRestaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
+
+      // render搜尋結果，若無符合結果render無符合頁面
+      if (filterRestaurants.length) {
+        res.render('index', { restaurants: filterRestaurants, keyword, title })
+      } else {
+        res.render('noMatchCase', { keyword })
+      }
+    })
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
